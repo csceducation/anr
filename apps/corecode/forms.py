f017e79d1aeae3c9c3ab20437690321d158a5b48
Logesh Kumar <130851416+logesh-works@users.noreply.github.com>
2024-06-09 18:57:11 +0530

    Updated time form

--- apps/corecode/forms.py
+++ apps/corecode/forms.py
@@ -50,12 +50,30 @@ class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = ["name"]
-class TimeForm(ModelForm):
-    prefix = "Time"
+class TimeForm(forms.ModelForm):
+    start_time = forms.TimeField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'placeholder': 'HH:MM'}, format='%H:%M'),
+        label='Start Time',
+        input_formats=['%H:%M']
+    )
+    end_time = forms.TimeField(
+        widget=forms.TimeInput(attrs={'type': 'time', 'placeholder': 'HH:MM'}, format='%H:%M'),
+        label='End Time',
+        input_formats=['%H:%M']
+    )
 
     class Meta:
         model = Time
-        fields = ["time"]
+        fields = []
+
+    def save(self, commit=True):
+        instance = super(TimeForm, self).save(commit=False)
+        start_time = self.cleaned_data.get('start_time')
+        end_time = self.cleaned_data.get('end_time')
+        instance.time = f"{start_time.strftime('%H:%M')} - {end_time.strftime('%H:%M')}"
+        if commit:
+            instance.save()
+        return instance
 class ExamForm(ModelForm):
     prefix = "Exam"
 
