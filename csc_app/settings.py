58eaf32b172c7c95a1dd51d81f27da50a182e5d8
Logesh Kumar <130851416+logesh-works@users.noreply.github.com>
2024-07-17 23:53:01 +0530

    added mysql

--- csc_app/settings.py
+++ csc_app/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = "csc_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
+
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'anrdb',
-		'USER': 'cscadmin',
+		'NAME': 'admin2_vdm',
+		'USER': 'admin2_cscadmin',
 		'PASSWORD': 'cscadmin@123',
 		'HOST':'localhost',
 		'PORT':'3306',
@@ -114,7 +114,7 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
